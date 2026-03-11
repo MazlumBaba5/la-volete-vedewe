bd58553c43e9b6907ed100ddb4c72e6d0f105507
MazlumBaba5 <claudiolapedota@gmail.com>
2026-03-11 15:09:17 -0500

    add age gate verification done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import AgeGateModal from '@/components/layout/AgeGateModal'
 
 export const metadata: Metadata = {
   title: {
@@ -18,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <AgeGateModal />
+      </body>
     </html>
   );
 }
